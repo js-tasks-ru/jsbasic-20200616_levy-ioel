45d28b3ecb9b759c6a7dacad39057773b3e7cc1c
levy.ioel@gmail.com <levy.ioel@gmail.com>
2020-07-11 16:26:34 -0700

    done

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,13 +3,6 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
-<<<<<<< HEAD
-    for (let i = 0; i < table.rows.length; i++) {
-        let row = table.rows[i];
-        let result = row.cells[i].style.backgroundColor = 'red';
-      }
-  }
-=======
   let rowsLength = table.rows.length;
   let rows = table.rows;
 
@@ -18,4 +11,3 @@ function makeDiagonalRed(table) {
     row.cells[i].style.backgroundColor = 'red';
   }
 }
->>>>>>> 388e124a4ec2f253d12de08da99063e390a6194b
