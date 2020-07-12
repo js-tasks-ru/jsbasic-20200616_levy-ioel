a16370c4a842c38a5672c4a158bbf746053b5d9c
levy.ioel@gmail.com <levy.ioel@gmail.com>
2020-07-11 18:31:13 -0700

    done

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,4 +5,11 @@
  */
 function makeFriendsList(friends) {
   // ваш код...
+  const ul = document.createElement('ul');
+
+  ul.innerHTML = friends.map(item => `
+      <li>${item.firstName} ${item.lastName}</li>
+  `).join('');
+
+  return ul;
 }
