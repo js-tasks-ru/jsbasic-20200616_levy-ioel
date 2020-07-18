9933445a521225fd70db493797304f4c1d6c2a1c
levy.ioel@gmail.com <levy.ioel@gmail.com>
2020-07-18 15:13:35 -0700

    changed a little

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,9 +1,10 @@
 function toggleText() {
   // ваш код...
-  const button = document.querySelector('button');
-            const elem = document.getElementById('text');
+  const button = document.querySelector('.toggle-text-button');
+            const elem = document.querySelector('#text');
             function reveal() {
+                // @ts-ignore
                 elem.hidden = !elem.hidden;
             }
-            button.addEventListener("click", reveal);
+            button.addEventListener('click', reveal);
 }
